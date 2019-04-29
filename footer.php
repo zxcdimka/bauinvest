4d1e005444603ae380cb28f0154af909441a363f
Dmitry <zxcdimka@gmail.com>
2019-04-29 11:54:54 +0300

    start project

---
+++ footer.php
@@ -0,0 +1,10 @@
+</body>
+
+<link rel="stylesheet" href="css/app.css">
+<link rel="stylesheet" href="assets/bootstrap-4.1.3/bootstrap.min.css">
+<link rel="stylesheet" href="assets/fontawesome-free-5.7.2-web/css/all.css">
+<link rel="stylesheet" href="assets/fancybox-master/dist/jquery.fancybox.min.css">
+<script src="assets/fancybox-master/dist/jquery.fancybox.js"
+<script src="js/js.js"></script>
+
+</html>
